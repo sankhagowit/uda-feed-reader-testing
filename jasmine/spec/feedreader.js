bca224b0880dde5c4402829605e70b3c21ca2cb4
Bobby <bobby@sankhagowit.com>
2017-03-08 00:09:13 -0600

    Made required modifications for resubmission

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
       });
 
 
-      /* TODO: Write a test that loops through each feed
+      /* Write a test that loops through each feed
        * in the allFeeds object and ensures it has a URL defined
        * and that the URL is not empty.
        */
@@ -38,7 +38,7 @@ $(function() {
       });
 
 
-      /* TODO: Write a test that loops through each feed
+      /* Write a test that loops through each feed
        * in the allFeeds object and ensures it has a name defined
        * and that the name is not empty.
        */
@@ -51,38 +51,38 @@ $(function() {
    });
 
 
-   /* TODO: Write a new test suite named "The menu" */
+   /* Write a new test suite named "The menu" */
    describe('The menu', function() {
 
-      /* TODO: Write a test that ensures the menu element is
+      /* Write a test that ensures the menu element is
        * hidden by default. You'll have to analyze the HTML and
        * the CSS to determine how we're performing the
        * hiding/showing of the menu element.
        */
       it('is hidden by default', function() {
-         expect($("body").hasClass("menu-hidden")).toBe(true);
+         expect($('body').hasClass('menu-hidden')).toBe(true);
       });
-      /* TODO: Write a test that ensures the menu changes
+      /* Write a test that ensures the menu changes
        * visibility when the menu icon is clicked. This test
        * should have two expectations: does the menu display when
        * clicked and does it hide when clicked again.
        */
       it('displays when clicked', function() {
-         $(".menu-icon-link").click();
+         $('.menu-icon-link').click();
 
-         expect($("body").hasClass("menu-hidden")).toBe(false);
+         expect($('body').hasClass('menu-hidden')).toBe(false);
       });
 
       it('hides when clicked again', function() {
-         $(".menu-icon-link").click();
+         $('.menu-icon-link').click();
 
-         expect($("body").hasClass("menu-hidden")).toBe(true);
+         expect($('body').hasClass('menu-hidden')).toBe(true);
       });
    });
-   /* TODO: Write a new test suite named "Initial Entries" */
+   /* Write a new test suite named "Initial Entries" */
    describe('Initial Entires', function() {
 
-      /* TODO: Write a test that ensures when the loadFeed
+      /* Write a test that ensures when the loadFeed
        * function is called and completes its work, there is at least
        * a single .entry element within the .feed container.
        * Remember, loadFeed() is asynchronous so this test will require
@@ -94,27 +94,36 @@ $(function() {
 
       it('has at least a single .entry element within .feed container', function() {
 
-         var children = $(".feed").children().children(".entry");
-			expect(children).toBeDefined();
+         var children = $('.feed').children().children('.entry');
 			expect(children.length).not.toBe(0);
       });
    });
 
-   /* TODO: Write a new test suite named "New Feed Selection" */
+   /* Write a new test suite named "New Feed Selection" */
    describe('New Feed Selection', function() {
 
-      /* TODO: Write a test that ensures when a new feed is loaded
+      /* Write a test that ensures when a new feed is loaded
        * by the loadFeed function that the content actually changes.
        * Remember, loadFeed() is asynchronous.
        */
       var oldContent;
       beforeEach(function(done) {
-         oldContent = $(".feed").children().children(".entry");
-         loadFeed(1, done);
+			jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+			loadFeed(0, function(){
+				oldHTML = document.getElementsByClassName('entry');
+				for(var i =0; i < oldHTML.length; i++){
+					oldContent += oldHTML[i].innerText;
+				}
+				loadFeed(1,done);
+			});
       });
 
       it('Changes Content', function() {
-         var newContent = $(".feed").children().children(".entry");
+         var newContent;
+			var newHTML = document.getElementsByClassName('entry');
+			for(var i =0; i < newHTML.length; i++){
+				newContent += newHTML[i].innerText;
+			}
          expect(newContent).not.toBe(oldContent);
       });
    });
